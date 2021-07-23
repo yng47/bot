587e26d27a7d2519cc03881822f42e59e3fb6ffd
yng47 <87718405+yng47@users.noreply.github.com>
2021-07-23 17:25:54 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -7,10 +7,13 @@ function addRow() {
   <div class="container">
   <div class="boxer">
    <div class="counter">
-	<a class="cr" href="index.html">[ 1 ]</a>
+	<a class="cr" href="test1.html">[ 1 ]</a>
 	<a class="cr" href="test2.html">[ 2 ]</a>
 	<a class="cr" href="test3.html">[ 3 ]</a>
 	<a class="cr" href="test4.html">[ 4 ]</a>
+	<a class="cr" href="test5.html">[ 5 ]</a>
+	<a class="cr" href="test6.html">[ 6 ]</a>
+	<a class="cr" href="test7.html">[ 7 ]</a>
 </div>
 </div>
 </div>
