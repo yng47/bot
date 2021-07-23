005ac8f5368de3075e4732543547783749b0cb3f
yng47 <87718405+yng47@users.noreply.github.com>
2021-07-23 04:17:13 +0300

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,21 @@
+function addRow() {
+  const div = document.createElement('div');
+
+  div.className = 'row';
+
+  div.innerHTML = `
+  <div class="container">
+  <div class="boxer">
+   <div class="counter">
+	<a class="cr" href="test1.html">[ 1 ]</a>
+	<a class="cr" href="test2.html">[ 2 ]</a>
+	<a class="cr" href="test3.html">[ 3 ]</a>
+	<a class="cr" href="test4.html">[ 4 ]</a>
+</div>
+</div>
+</div>
+  `;
+
+  document.getElementById('pagecounter').appendChild(div);
+}
+
