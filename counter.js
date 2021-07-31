55194338d260fd0b5eaeb43cc8d015a35e2b541c
yng47 <87718405+yng47@users.noreply.github.com>
2021-07-31 17:02:56 +0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -17,6 +17,17 @@ function addRow() {
 	<a class="cr" href="test8.html">[ 8 ]</a>
 	<a class="cr" href="test9.html">[ 9 ]</a>
 	<a class="cr" href="test10.html">[ 10 ]</a>
+	<a class="cr" href="test11.html">[ 11 ]</a>
+	<a class="cr" href="test12.html">[ 12 ]</a>
+	<a class="cr" href="test13.html">[ 13 ]</a>
+	<a class="cr" href="test14.html">[ 14 ]</a>
+	<a class="cr" href="test15.html">[ 15 ]</a>
+	<a class="cr" href="test16.html">[ 16 ]</a>
+	<a class="cr" href="test17.html">[ 17 ]</a>
+	<a class="cr" href="test18.html">[ 18 ]</a>
+	<a class="cr" href="test19.html">[ 19 ]</a>
+	<a class="cr" href="test20.html">[ 20 ]</a>
+	<a class="cr" href="test21.html">[ 21 ]</a>
 </div>
 </div>
 </div>
